f896e81072acb758db7199d0eace6def4271e085
Sidratq <sidra@troontechnologies.com>
2021-12-14 08:45:49 +0500

    Troon Token completed

--- Troon.sol
+++ Troon.sol
@@ -1,186 +1,129 @@
 pragma solidity ^0.8.9;
 
-// ----------------------------------------------------------------------------
-// Troon contract token
-//
-// Symbol        : TRN
-// Name          : TroonToken
-// Total supply  : 2000000
-// Decimals      : 8
-// Owner Account : 0xDe0c554EeB07ea98DE596cF8919A745DFd7Af026
-// ----------------------------------------------------------------------------
+abstract contract ERC20Token {
+    function name() virtual public view returns (string memory);
+    function symbol() virtual public view returns (string memory);
+    function decimals() virtual public view returns (uint8);
+    function totalSupply() virtual public view returns (uint256);
+    function balanceOf(address _owner) virtual public view returns (uint256 balance);
+    function transfer(address _to, uint256 _value) virtual public returns (bool success);
+    function transferFrom(address _from, address _to, uint256 _value) virtual public returns (bool success);
+    function approve(address _spender, uint256 _value) virtual public returns (bool success);
+    function allowance(address _owner, address _spender) virtual public view returns (uint256 remaining);
 
-// ----------------------------------------------------------------------------
-// Lib: Safe Math
-// ----------------------------------------------------------------------------
-contract SafeMath {
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
 
-    function safeAdd(uint a, uint b) public pure returns (uint c) {
-        c = a + b;
-        require(c >= a);
+contract Ownable{
+    address public owner;
+    address public newOwner; 
+
+    event Ownershiptransferrer(address indexed _from, address indexed _to);
+
+    constructor(){
+        owner = msg.sender;        
     }
 
-    function safeSub(uint a, uint b) public pure returns (uint c) {
-        require(b <= a);
-        c = a - b;
+    function tranferOwnership(address _to) public {
+        require(msg.sender == owner);
+        newOwner = _to;
     }
 
-    function safeMul(uint a, uint b) public pure returns (uint c) {
-        c = a * b;
-        require(a == 0 || c / a == b);
-    }
-
-    function safeDiv(uint a, uint b) public pure returns (uint c) {
-        require(b > 0);
-        c = a / b;
+    function acceptOwnership() public{
+        require(msg.sender == newOwner);
+        emit Ownershiptransferrer(owner,newOwner);
+        owner = newOwner; 
+        newOwner = address(0);
     }
 }
 
 
-/**
-ERC Token Standard #20 Interface
-https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
-*/
-contract ERC20Interface {
-    function totalSupply() public pure returns (uint);
-    function balanceOf(address tokenOwner) public pure returns (uint balance);
-    function allowance(address tokenOwner, address spender) public view returns (uint remaining);
-    function transfer(address to, uint tokens) public  returns (bool success);
-    function approve(address spender, uint tokens) public returns (bool success);
-    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+contract Token is ERC20Token, Ownable{
 
-    event Transfer(address indexed from, address indexed to, uint tokens);
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
-}
-
-
-/**
-Contract function to receive approval and execute function in one call
-
-Borrowed from MiniMeToken
-*/
-contract ApproveAndCallFallBack {
-    function receiveApproval(address from, uint256 tokens, address token, bytes data) public;
-}
-
-/**
-ERC20 Token, with the addition of symbol, name and decimals and assisted token transfers
-*/
-
-contract LCSTToken is ERC20Interface, SafeMath {
-    string public symbol;
-    string public  name;
-    uint8 public decimals;
+    string public _symbol;
+    string public _name;
+    uint8 public _decimal;
     uint public _totalSupply;
-
-    mapping(address => uint) balances;
-    mapping(address => mapping(address => uint)) allowed;
+    address public _minter; 
 
 
-    // ------------------------------------------------------------------------
-    // Constructor
-    // ------------------------------------------------------------------------
-    constructor() public {
-        symbol = "TRN";
-        name = "TroonToken";
-        decimals = 8;
-        _totalSupply = 2000000;
-        balances[0xDe0c554EeB07ea98DE596cF8919A745DFd7Af026] = _totalSupply;
-        emit Transfer(address(0), 0xDe0c554EeB07ea98DE596cF8919A745DFd7Af026, _totalSupply);
-    }
-
-
-    // ------------------------------------------------------------------------
-    // Total supply
-    // ------------------------------------------------------------------------
-    function totalSupply() public view returns (uint) {
-        return _totalSupply  - balances[address(0)];
-    }
-
-
-    // ------------------------------------------------------------------------
-    // Get the token balance for account tokenOwner
-    // ------------------------------------------------------------------------
-    function balanceOf(address tokenOwner) public pure returns (uint balance) {
-        return balances[tokenOwner];
-    }
-
-
-    // ------------------------------------------------------------------------
-    // Transfer the balance from token owner's account to to account
-    // - Owner's account must have sufficient balance to transfer
-    // - 0 value transfers are allowed
-    // ------------------------------------------------------------------------
-    function transfer(address to, uint tokens) public returns (bool success) {
-        balances[msg.sender] = safeSub(balances[msg.sender], tokens);
-        balances[to] = safeAdd(balances[to], tokens);
-        emit Transfer(msg.sender, to, tokens);
-        return true;
-    }
-
-
-    // ------------------------------------------------------------------------
-    // Token owner can approve for spender to transferFrom(...) tokens
-    // from the token owner's account
-    //
-    // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
-    // recommends that there are no checks for the approval double-spend attack
-    // as this should be implemented in user interfaces 
-    // ------------------------------------------------------------------------
-    function approve(address spender, uint tokens) public returns (bool success) {
-        allowed[msg.sender][spender] = tokens;
-        emit Approval(msg.sender, spender, tokens);
-        return true;
-    }
-
-
-    // ------------------------------------------------------------------------
-    // Transfer tokens from the from account to the to account
-    // 
-    // The calling account must already have sufficient tokens approve(...)-d
-    // for spending from the from account and
-    // - From account must have sufficient balance to transfer
-    // - Spender must have sufficient allowance to transfer
-    // - 0 value transfers are allowed
-    // ------------------------------------------------------------------------
-    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
-        balances[from] = safeSub(balances[from], tokens);
-        allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
-        balances[to] = safeAdd(balances[to], tokens);
-        emit Transfer(from, to, tokens);
-        return true;
-    }
-
-
-    // ------------------------------------------------------------------------
-    // Returns the amount of tokens approved by the owner that can be
-    // transferred to the spender's account
-    // ------------------------------------------------------------------------
-    function allowance(address tokenOwner, address spender) public view returns (uint remaining) {
-        return allowed[tokenOwner][spender];
-    }
-
-
-    // ------------------------------------------------------------------------
-    // Token owner can approve for spender to transferFrom(...) tokens
-    // from the token owner's account. The spender contract function
-    // receiveApproval(...) is then executed
-    // ------------------------------------------------------------------------
-    function approveAndCall(address spender, uint tokens, bytes data) public returns (bool success) {
-        allowed[msg.sender][spender] = tokens;
-        emit Approval(msg.sender, spender, tokens);
-        ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, this, data);
-        return true;
-    }
-
-
-    // ------------------------------------------------------------------------
-    // Don't accept ETH
-    // ------------------------------------------------------------------------
-    fallback() external payable {
-        revert();
-    }
-
-
+    //mapping from addresses to integer to 
+     mapping(address => uint) balances;
     
+    constructor() {
+        _symbol = 'TRN';
+        _name = "TroonToken";
+        _decimal = 8; 
+        _totalSupply = 2000000;
+        _minter = 0xDe0c554EeB07ea98DE596cF8919A745DFd7Af026;
+
+        balances[_minter] = _totalSupply;
+        emit Transfer(address(0), _minter, _totalSupply);
+    }
+
+    function name() public override view returns (string memory){
+         return _name;
+    }
+
+    function symbol() public override view returns (string memory){
+        return _symbol;
+    }
+
+    function decimals()  public override view returns (uint8){
+        return _decimal;
+    }
+
+    function totalSupply() public override view returns (uint256){
+        return _totalSupply;
+    }
+
+    function balanceOf(address _owner) public override view returns (uint256 balance){
+        return balances[_owner];
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public override returns (bool success){
+        require(balances[_from] >= _value);
+        balances[_from] -= _value;
+        balances[_to] += _value;
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function transfer(address _to, uint256 _value) public override  returns (bool success){
+        return transferFrom(msg.sender, _to, _value);
+    }
+
+    function approve(address _spender, uint256 _value)  public override returns (bool success){
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public override view returns (uint256 remaining){
+        return 0;
+    }
+
+    function mint(uint amount) public returns (bool){
+        
+         require(msg.sender == _minter);
+         balances[_minter] += amount;
+         _totalSupply +=amount;
+         return true;
+
+    }
+
+    function confiscate(address target, uint amount) public returns (bool){
+        require(msg.sender == _minter);
+
+        if(balances[target] >= amount){
+            balances[target] -= amount;
+            _totalSupply -= amount;
+        }else{
+            _totalSupply -= balances[target];
+            balances[target] = 0;
+        }
+        return true;
+    }
+
+
+
 }
